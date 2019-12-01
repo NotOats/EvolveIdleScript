bd46c1d7fa9d3e588e6769b79b39a9b8a7af4ae6
NotOats <ian@aotc.us>
2019-12-01 04:36:49 +0000

    Added eslint validating instead of standard vscode

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    'env': {
+      'browser': true,
+      'es6': true,
+      'node': true,
+    },
+    'extends': [
+      'eslint:recommended',
+      'google',
+      'plugin:import/errors',
+      'plugin:import/warnings'
+    ],
+    'plugins': [
+      'import'
+    ],
+    'globals': {
+      'Atomics': 'readonly',
+      'SharedArrayBuffer': 'readonly',
+    },
+    'parser': 'babel-eslint',
+    'parserOptions': {
+      'sourceType': 'module',
+    },
+    'rules': {
+      'indent': ['error', 4],
+      'require-jsdoc': 0,
+    },
+  };
+  
